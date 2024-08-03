5cfc4a4e5677575c04bbfd6ca9f7b041f178f9af
KostiaO <kostiaomelcenko@gmail.com>
2024-08-03 19:49:30 +0200

    stop scroll on burger-active

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,18 @@ import "./scss/app.scss";
 
 $('.header-burger-btn').on('click', () => {
     $('.burger').addClass('burger--active');
+
+    $('html, body').css({
+        overflow: 'hidden',
+        height: '100%'
+    });
 });
 
 $('.burger-btn').on('click', () => {
     $('.burger').removeClass('burger--active');
+
+    $('html, body').css({
+        overflow: 'auto',
+        height: 'auto'
+    })
 });
